907cb52377249d7722e9a83fe78c50874eb9b370
Marcin Mulawa <44370984+marcinmul@users.noreply.github.com>
2019-01-02 22:51:53 +0100

    Create Source.cpp
    marcin

---
+++ ciazowy_specjalista/ciazowy_specjalista/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	float n, x, wiek, czas, ile;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> wiek >> czas >> ile;
+		x = (czas * 12 * ile - wiek * 12 - czas * 12) / (ile - 1);
+		cout << round(abs(x))<<endl;
+	}
+}
